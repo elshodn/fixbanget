ce009d9a500285c089ee934a89334a608c4af974
Al-Muhsinjon <muhsinjonmullajonov@gmail.com>
2025-05-28 17:58:53 +0500

    Test bus

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 import { getTelegramUserId } from "@/lib/telegram";
 import React from "react";
 
-export const TestPage = () => {
+const TestPage = () => {
   const telegramID = getTelegramUserId();
   return (
     <div className="size-24 bg-red-300 text-2xl font-bold ">
@@ -10,3 +10,5 @@ export const TestPage = () => {
     </div>
   );
 };
+
+export default TestPage;
